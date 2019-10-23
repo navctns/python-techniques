30df09144dd8affc7395466405eaadf776ae593b
navctns <53227832+navctns@users.noreply.github.com>
2019-10-23 13:02:02 +0530

    Add files via upload

---
+++ inverted_pyramid.py
@@ -0,0 +1,13 @@
+#inverted half pyramid
+for i in range(11):
+    print(i*" "+abs(i-10)*"7"+"\n")
+    #print("\n")
+    #sp=i*" "
+"""
+    print("_"*i)
+    #print(sp)
+    for j in range(abs(i-10)+1):
+        print("7",end='')
+    #print("\n")
+        """    
+        
